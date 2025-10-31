9c552607936f03329e14d68206fbd0f6d86c3184
Amund101 <amundbwold@gmail.com>
2025-10-31 14:06:05 +0100

    auto-commit

---
+++ IT1/Kapittel 3/3C/Oppgaver_11til23/3Coppg.js
@@ -0,0 +1,31 @@
+let i=0
+while(i<=50){
+    console.log(i);
+    i++;
+}
+
+for(let i=0; i<=50; i++){
+    console.log(i);
+}
+
+let i=0
+while(i<=999){
+    console.log(i)
+    i+=3
+}
+
+
+for(let i=2; i<=100; i+=2){
+    console.log(i)
+}
+
+let sum = 0;
+for(let i=0; i<=100; i++){
+    sum+=i
+}
+console.log(sum);
+
+
+// 2 //
+
+

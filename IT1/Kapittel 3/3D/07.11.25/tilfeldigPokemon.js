e16f4015119c562699f9b08a4f2f358c9d3364eb
Amund101 <amundbwold@gmail.com>
2025-11-10 08:24:59 +0100

    Auto commit at 2025-11-10 08:24:59

--- IT1/Kapittel 3/3D/07.11.25/tilfeldigPokemon.js
+++ IT1/Kapittel 3/3D/07.11.25/tilfeldigPokemon.js
@@ -1,30 +1,28 @@
-let numberOfWaterPokemon = 0
-let numberOfFirePokemon = 0
-let numberOfGrassPokemon = 0
+let numberOfWaterPokemon = 0;
+let numberOfFirePokemon = 0;
+let numberOfGrassPokemon = 0;
 
 function getPokemonType(id) {
-    let chance = Math.floor(Math.Random()*3)
+    let chance = Math.floor(Math.random() * 3);
 
-    for (let id = 0; id < 10; id++) {
-        let type = getPokemonType(id)  
-        if(chance == 0) {
-        numberOfWaterPokemon += 1
-        console.log("Pokemon ID ["+id+"] er av typen Water. Den elsker å svømme!")
-            return "water"
-        }
-        else if(chance ==1) {
-            numberOfFirePokemon += 1
-            console.log("Pokemon ID ["+id+"] er av typen Fire. Vær forsiktig, den er varm!")
-                return "fire"
-        }
-        else {
-            numberOfGrassPokemon += 1
-        console.log("Pokemon ID ["+id+"] er av typen Grass. Den nyter solen!")
-            return "grass"
-        }
+    if (chance === 0) {
+        numberOfWaterPokemon++;
+        console.log(`Pokemon ID [${id}] er av typen Water. Den elsker å svømme!`);
+    } 
+    else if (chance === 1) {
+        numberOfFirePokemon++;
+        console.log(`Pokemon ID [${id}] er av typen Fire. Vær forsiktig, den er varm!`);
+    } 
+    else {
+        numberOfGrassPokemon++;
+        console.log(`Pokemon ID [${id}] er av typen Grass. Den nyter solen!`);
     }
-
-    console.log(`Vi har ${numberOfWaterPokemon} water pokemon`)
-    console.log(`Vi har ${numberOfFirePokemon} fire pokemon`)
-    console.log(`Vi har ${numberOfGrassPokemon} grass pokemon`)
 }
+
+for (let id = 0; id < 10; id++) {
+    getPokemonType(id);
+}
+
+console.log(`Vi har ${numberOfWaterPokemon} water Pokémon`);
+console.log(`Vi har ${numberOfFirePokemon} fire Pokémon`);
+console.log(`Vi har ${numberOfGrassPokemon} grass Pokémon`);

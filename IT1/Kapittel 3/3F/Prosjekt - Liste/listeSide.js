a871a8e7d0ec02c094b065b6b57d323fa239e760
Amund101 <amundbwold@gmail.com>
2026-01-16 11:18:09 +0100

    Auto commit at 2026-01-16 11:18:09

--- IT1/Kapittel 3/3F/Prosjekt - Liste/listeSide.js
+++ IT1/Kapittel 3/3F/Prosjekt - Liste/listeSide.js
@@ -1,3 +1,21 @@
+  console.log("listeSide.js loaded");
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyApXFO2sVagrm4LYwA9dcui6DMUdRKH4Vk",
+    authDomain: "prosjekt-1-4f037.firebaseapp.com",
+    projectId: "prosjekt-1-4f037",
+    storageBucket: "prosjekt-1-4f037.firebasestorage.app",
+    messagingSenderId: "619968747857",
+    appId: "1:619968747857:web:814a97191fe8204f789b70"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 
 // Bytte mellom "sider"
